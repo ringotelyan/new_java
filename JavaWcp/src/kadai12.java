04c0dfb516797077a80a2273a803b41e2530f729
ringotelyan <sute.dh9999@gmail.com>
2022-08-25 18:03:09 +0900

    [Update]課題２途中

--- JavaWcp/src/kadai12.java
+++ JavaWcp/src/kadai12.java
@@ -1,5 +1,15 @@
 
 // 課題1-2）2から100までの偶数のみを全て足す計算プログラムを作成
 public class kadai12 {
+	public static void main(String[] args) {
+		int number = 100;
+		int sum = 0;
+		
+		for (int i = 2; i <= number; i++) {
+			if (number[i] % 2 == 0) {
+				sum = sum + i;
+			}
+		}
+	}
 
 }

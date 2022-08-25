bc7c744772379de1658dc7847483aba1cb1218c0
ringotelyan <sute.dh9999@gmail.com>
2022-08-25 21:57:01 +0900

    [Update]訂正

--- JavaWcp/src/kadai11.java
+++ JavaWcp/src/kadai11.java
@@ -2,11 +2,10 @@
 // 課題1-1）1から100までの整数をすべて足す計算プログラムを作成
 public class kadai11 {
 	public static void main(String[] args) {
-		int n = 100;
 		int sum = 0;
 		
-		for (int i = 1; i <= n; i++) {
-			sum = sum + i;
+		for (int i = 1; i <= 100; i++) {
+			sum += i;
 		}
 		System.out.println("1から100までの整数をすべて足した和は" + sum);
 	}

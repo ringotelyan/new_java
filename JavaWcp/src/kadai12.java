65c422aa60fc1c42981a5f97bd13bcb791c734a9
ringotelyan <sute.dh9999@gmail.com>
2022-08-25 21:55:52 +0900

    [Update]課題完了

--- JavaWcp/src/kadai12.java
+++ JavaWcp/src/kadai12.java
@@ -2,14 +2,14 @@
 // 課題1-2）2から100までの偶数のみを全て足す計算プログラムを作成
 public class kadai12 {
 	public static void main(String[] args) {
-		int number = 100;
 		int sum = 0;
 		
-		for (int i = 2; i <= number; i++) {
-			if (number[i] % 2 == 0) {
-				sum = sum + i;
+		for (int i = 2; i <= 100; i++) {
+			if (i % 2 == 0) {
+				sum += i;
 			}
 		}
+		System.out.println("2から100までの偶数の和は" + sum);
 	}
 
 }

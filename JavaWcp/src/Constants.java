476cde573bf3e515cad2ec47ca268206a73cf30a
ringotelyan <sute.dh9999@gmail.com>
2022-08-26 16:06:09 +0900

    [Update]応用第1章

--- JavaWcp/src/Constants.java
+++ JavaWcp/src/Constants.java
@@ -1,8 +1,15 @@
 
 public class Constants {
-	public static final String CAT_NAME = "猫";
-	public static final int CAT_LEG_COUNT = 4;
-	public static final String OCTOPUS_NAME ="タコ";
-	public static final int OCTOPUS_LEG_COUNT = 8;
+	// 内部クラスCatの中に、猫関係の定数を定義
+	public static class Cat {
+		public static final String CAT_NAME = "猫";
+		public static final int CAT_LEG_COUNT = 4;
+	}
+	// 内部クラスOctopusの中に、タコ関係の定数を定義
+	public static class Octopus {
+		public static final String OCTOPUS_NAME ="タコ";
+		public static final int OCTOPUS_LEG_COUNT = 8;
+	}
+	
 
 }

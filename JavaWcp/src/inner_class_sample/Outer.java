50ab27ff7fedf8c0a667bd301aa0bc213e4d3600
ringotelyan <sute.dh9999@gmail.com>
2022-08-26 16:04:04 +0900

    [Update]応用第1章

--- JavaWcp/src/inner_class_sample/Outer.java
+++ JavaWcp/src/inner_class_sample/Outer.java
@@ -1,8 +1,16 @@
 package inner_class_sample;
 
 public class Outer {
-	public static class Inner {
-		
+	// 外部クラスに定義したフィールドgreeting
+	// staticなフィールドgreeting
+	private static String greeting = "こんにちは";
+	// staticな内部クラスInner
+	public class static Inner {
+		public String hello() {
+			// 内部クラスInnerから、外部クラスOuterのフィールドgreetingを参照
+			// 外部クラスOuterのフィールドgreetingはstaticなので、参照可能
+			return greeting;
+		}
 	}
 
 }

a0d6d6d76499540ea134d9c86608fe818c1bb4eb
ringotelyan <sute.dh9999@gmail.com>
2022-08-26 14:24:00 +0900

    [Update]応用第1章

--- JavaWcp/src/Example.java
+++ JavaWcp/src/Example.java
@@ -1,10 +1,9 @@
 import inner_class_sample.Outer;
-import inner_class_sample.Outer.Inner;
 
 public class Example {
 	public static void main(String[] args) {
 		Outer outer = new Outer(); // クラスOuterをインスタンス化
-		Outer.Inner inner = outer.new Inner(); // クラスOuterの内部クラスInnerをインスタンス化
+		Outer.Inner inner = new Outer.Inner(); // クラスOuterの内部クラスInnerをインスタンス化
 	} 
 
 }

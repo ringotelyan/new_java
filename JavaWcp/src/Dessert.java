64234de91deb230fb979c422f12bc2d556d39967
ringotelyan <sute.dh9999@gmail.com>
2022-08-28 18:08:57 +0900

    [Update]応用第２章

--- JavaWcp/src/Dessert.java
+++ JavaWcp/src/Dessert.java
@@ -1,16 +1,35 @@
 
 public enum Dessert {
-	// それぞれに値段を指定する
-	APPLE(100), ICE_CREAM(200), CAKE(300),;
-	// 値段を保持するフィールド
+	APPLE(100) {
+		@Override
+		void advertise() {
+			// リンゴ用のadvertiseの実装
+			System.out.println("まっかなりんごが" + getPrice() + "円!");
+		}
+	},
+	ICE_CREAM(200) {
+		@Override
+		void advertise() {
+			// アイスクリーム用のadvertiseの実装
+			System.out.println("夏にぴったりのアイスクリームが" + getPrice() + "円!");
+		}
+	},
+	CAKE(300) {
+		@Override
+		void advertise() {
+			// ケーキ用のadvertiseの実装
+			System.out.println("皆大好き甘いケーキが" + getPrice() + "円!");
+		}
+	},
+	;
 	private int price;
-	// コンストラクタを追加。引数はint型の値段
 	private Dessert(int price) {
 		this.price = price;
 	}
-	// 値段を取得するメソッドを追加
 	public int getPrice() {
 		return price;
 	}
-
+	// 宣伝文句を出力する中小メソッド。各インスタンスで実装する
+	abstract void advertise();
+	
 }
---
+++ JavaWcp/src/jikken.java
@@ -0,0 +1,7 @@
+
+public class jikken {
+	public static void main(String[] args) {
+		System.out.println("実験");
+	}
+
+}

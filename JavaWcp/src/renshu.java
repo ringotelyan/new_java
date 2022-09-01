32c98855cbe55e7efbd6abc4377a9c1e626062ae
ringotelyan <sute.dh9999@gmail.com>
2022-09-01 15:04:55 +0900

    [Finish]基礎編第3章

--- JavaWcp/src/renshu.java
+++ JavaWcp/src/renshu.java
@@ -1,4 +1,14 @@
 
-public class renshu {
+import java.util.HashuMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
 
-}
+Set<Integer> set = new LinkedHashSet<>();
+set.add(1); // OK
+set.add("Two"); // コンパイルエラー
+
+Map<Integer, String> map = new HashMap<>();
+map.put(1, "Taro"); // OK
+map.put(2, "Hanako"); // OK
+int value = map.get(0); // コンパイルエラー

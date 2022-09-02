4c43e4ac3ab9683d3e66e30f43c78f3cfd4ac200
ringotelyan <sute.dh9999@gmail.com>
2022-09-02 10:28:46 +0900

    [Finish]基礎第４章

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,16 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		final int LUCKY_NUMBER= 7;
+		String str1;
+		str1 = "ringotelyan";
+		System.out.println(str1);
+		
+		str1 = "ごはん";
+		System.out.println(str1);
+		System.out.println(LUCKY_NUMBER);
+		
+		
+	}
+
+}
